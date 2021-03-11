33c70fc148bfd1768b0e8c223e3a019547bb1832
LuukFTF <lucasvdvegt@gmail.com>
2021-03-11 15:09:53 +0100

    added backend-mvc structure

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+    <title>KPM - web</title>
+    <script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+</body>
+</html>
