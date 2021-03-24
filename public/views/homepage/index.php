f5a2cd004c4d49a3f203b66e8bda0fe5ab31238f
Rob de Willigen <robdewilligen@outlook.com>
2021-03-24 13:45:30 +0100

    added css

---
+++ public/views/homepage/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../../css/style.css"/>
+    <title>KPM - web</title>
+    <script type="text/javascript" src="../../js/main.js"></script>
+</head>
+<body>
+
+    <header class="header">
+        <h4>header</h4>
+    </header>
+
+    <div class="body">
+        <h4>body</h4>
+    </div>
+
+    <footer class="footer">
+        <h4>footer</h4>
+    </footer>
+</body>
+</html>
